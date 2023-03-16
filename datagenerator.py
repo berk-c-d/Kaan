1fedde694cb00a60186b9504d1070040b590dc2b
kaangokbayrak <gokbayrakkaan@gmail.com>
2023-03-16 13:40:25 +0300

    1

--- datagenerator.py
+++ datagenerator.py
@@ -1,25 +1,26 @@
-import random
+import numpy as np
 
-data = open('Data.csv','w')
+data = open('Data.csv', 'w')
 data.write("ID,Weight,Revenue,X_loc,Y_loc\n")
 
-amount_of_item=50
-min_weight=2
-max_weight=10
-min_revenue=10
-max_revenue=300
-map_size=50
+amount_of_item = 50
+min_weight = 2
+max_weight = 10
+min_revenue = 10
+max_revenue = 300
+map_size = 50
 
-random.seed(1)
+np.random.seed(1)
+
+weights = np.random.randint(min_weight, max_weight + 1, size=amount_of_item)
+revenues = np.random.randint(min_revenue, max_revenue + 1, size=amount_of_item)
+x_locs = np.random.randint(0, map_size, size=amount_of_item)
+y_locs = np.random.randint(0, map_size, size=amount_of_item)
 
 for i in range(amount_of_item):
-    Weight=str(random.randint(min_weight, max_weight))
-    Revenue =str( random.randint(min_revenue, max_revenue))
-    X_loc=str(random.randint(0, map_size))
-    Y_loc =str( random.randint(0, map_size))
+    Weight = str(weights[i])
+    Revenue = str(revenues[i])
+    X_loc = str(x_locs[i])
+    Y_loc = str(y_locs[i])
 
-    data.write(str(i)+","+Weight+","+Revenue+","+X_loc+","+Y_loc+"\n")
-
-
-
-
+    data.write(str(i) + "," + Weight + "," + Revenue + "," + X_loc + "," + Y_loc + "\n")
--- distancemap.py
+++ distancemap.py
@@ -1,50 +1,30 @@
-from tkinter.filedialog import askopenfile
+import pandas as pd
 from functions import *
+from tkinter.filedialog import askopenfile
 
-
-file=askopenfile(mode='r', title='Lütfen birleştirme bilgilerinin bulunduğu dosyayı seçin')
-lines=file.readlines()
+file = askopenfile(mode='r', title='Lütfen birleştirme bilgilerinin bulunduğu dosyayı seçin')
+lines = file.readlines()
 
 lines.remove(lines[0])
 
-#print(lines)
-int_lines=[]
+int_lines = []
 for line in lines:
-    #print(line.replace("\n","").split(","))
-    line=line.replace("\n","").split(",")
-    int_line=[int(x) for x in line]
+    line = line.replace("\n", "").split(",")
+    int_line = [int(x) for x in line]
     int_lines.append(int_line)
 
-    #print(int_line)
-
-distance_map=[]
-temp_list=[]
-Big_M=99999999;
+distance_map = []
+temp_list = []
+Big_M = 999;
 for i in range(len(lines)):
     temp_list = []
     for j in range(len(lines)):
-        if i==j:
+        if i == j:
             temp_list.append(Big_M)
         else:
-            temp_list.append(euclidian_dist(int_lines,i,j))
+            temp_list.append(euclidian_dist(int_lines, i, j))
     distance_map.append(temp_list)
 
-
-print(distance_map)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Export to excel
+df = pd.DataFrame(distance_map)
+df.to_excel('distance_maps.xlsx', index=True)
